da875e0272f23b9cb08af3420d845dd8d9011fdc
Subin Sebastian <subin.sebastian457@gmail.com>
2022-11-24 12:29:21 +0530

    added: starter challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { timer, BehaviorSubject, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'tailwindcss-angular-starter-project';
+  START_TIMER_AT = 10;
+
+  interval$ = timer(0, 1000).pipe(tap(console.log));
+
+  missionStatus$ = new BehaviorSubject<
+    'yet to start' | 'started' | 'aborted' | 'liftoff'
+  >('yet to start');
+
+  countdown$ = of(this.START_TIMER_AT);
 }
